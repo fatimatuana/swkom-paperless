f0a5f3e8547ed0a82c390c9c6a72a08979db5325
fatima tuana <fatima__tuana@hotmail.com>
2024-02-15 13:55:27 +0100

    added DI for document (but no rules yet)

---
+++ sooooooon/out/csharp/Business/Concrete/DocumentManager.cs
@@ -0,0 +1,52 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class DocumentManager : IDocumentService
+    {
+
+        IDocumentDal _documentDal;
+
+        public DocumentManager(IDocumentDal documentDal)
+        {
+            _documentDal = documentDal;
+        }
+
+        public void Add(Document document)
+        {
+            _documentDal.Add(document);
+        }
+
+        public void Delete(Document document)
+        {
+            _documentDal.Delete(document);
+        }
+
+        public List<Document> Getall()
+        {
+            return _documentDal.GetAll();
+        }
+
+        public Document GetById(int id)
+        {
+            return _documentDal.Get(i => i.Id == id);            
+        }
+
+        public Document GetByTitle(string title)
+        {
+            return _documentDal.Get(t => t.Title == title);
+        }
+
+        public void Update(Document document)
+        {
+            _documentDal.Update(document);
+        }
+    }
+}
